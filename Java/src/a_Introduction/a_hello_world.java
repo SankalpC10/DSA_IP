01a84ebeb7f46f8d6bb59b3dbc971735ba1c2ba6
SankalpC10 <sgchavhan_b18@et.vjti.ac.in>
2023-04-02 00:08:22 +0530

    Added Hello World File

--- Java/src/a_Introduction/a_hello_world.java
+++ Java/src/a_Introduction/a_hello_world.java
@@ -1,8 +1,19 @@
 package a_Introduction;
 
+// Java Hello World Program -- single Line Comment
 public class a_hello_world {
-    public static void main(String[] args) {
+    public static void main(String[] args) { /* public is access specifier -- main function get accessible from everywhere
+                                              * static is a special method which can work even without creating an object.
+                                              * void is the return type
+                                              * String[] args allows us to pass arguments on command line
+                                              */
+        /*
+            This is an example
+            of multiline Comment
+            CLI Commands
+            $ javac a_hello_world.java - to build
+            $ java a_hello_world
+        */
         System.out.println("Hello World");
-
     }
 }
