3bef2b745fb1f12e946ce31db9b7ed447740808a
SankalpC10 <sgchavhan_b18@et.vjti.ac.in>
2023-04-29 17:06:05 +0530

    adding files

--- Java/src/a_Introduction/j_escapeSequence.java
+++ Java/src/a_Introduction/j_escapeSequence.java
@@ -1,2 +1,11 @@
-package a_Introduction;public class j_escapeSequence {
+package a_Introduction;
+
+public class j_escapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Welcome to \"Area 51\"!");
+        System.out.println("Welcome to 'Miami Vice'!");
+        System.out.println("Hi \nWelcome Here");
+        // valid \ sequences are \t-tab,\b,\n,\r,\f,\',\",\\
+        System.out.println("C:\\xyz\\abc.txt");
+    }
 }
